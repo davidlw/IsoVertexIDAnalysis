d4724f28e7e6f3c6f456b8f5c74b23301b02febf
Austin <abaty@rice.edu>
2020-02-01 01:21:45 -0600

    huge updates for MC

--- IsoVertexIDAnalyzer/plugins/IsoVertexIDTreeAnalyzer.cc
+++ IsoVertexIDAnalyzer/plugins/IsoVertexIDTreeAnalyzer.cc
@@ -22,9 +22,11 @@
 #include <exception>
 #include <iostream>
 #include <memory>
+#include <algorithm>
 #include <math.h>
 #include <vector>
 #include <string>
+#include <map>
 
 // user include files
 #include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
@@ -39,6 +41,16 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include "SimDataFormats/Associations/interface/TrackToTrackingParticleAssociator.h"
+#include "SimDataFormats/Associations/interface/VertexToTrackingVertexAssociator.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h"
+#include "SimTracker/Records/interface/TrackAssociatorRecord.h"
+#include "SimTracker/Records/interface/TrackAssociatorRecord.h"
+#include "DataFormats/RecoCandidate/interface/TrackAssociation.h"
+
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingVertex.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "DataFormats/Common/interface/Handle.h"
@@ -77,14 +89,24 @@ class IsoVertexIDTreeAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedReso
       explicit IsoVertexIDTreeAnalyzer(const edm::ParameterSet&);
       ~IsoVertexIDTreeAnalyzer();
 
+      long trkIdMaker( float pt, float eta, float phi, float vz, float chi2 );
+
       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
    private:
 
       edm::Service<TFileService> theOutputs;
       edm::EDGetTokenT<reco::TrackCollection> token_tracks;
+      edm::EDGetTokenT<edm::View<reco::Track> > token_edmViewTracks;
       edm::EDGetTokenT<reco::VertexCollection> token_vertices;
-      edm::EDGetTokenT<std::vector< PileupSummaryInfo > > token_genPU;   
+//    edm::EDGetTokenT<std::vector< PileupSummaryInfo > > token_genPU;   
+      edm::EDGetTokenT< std::vector< TrackingVertex> >token_TrackingVtx;
+      edm::EDGetTokenT< std::vector< TrackingParticle> >token_TrackingParticle;
+
+      //trking particle association maps
+      edm::EDGetTokenT<reco::RecoToSimCollection> associatorMapRTS_;
+      edm::EDGetTokenT<reco::SimToRecoCollection> associatorMapSTR_;
+
 
       TTree* vertexTree;
 
@@ -106,6 +128,11 @@ class IsoVertexIDTreeAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedReso
       float zVtxErr[NMAXVTX];
       float chi2Vtx[NMAXVTX];
       int   ndofVtx[NMAXVTX];
+      float minZSepReco[NMAXVTX];
+      int   bestMatchGenByWeight[NMAXVTX];
+      float contaminationFracByWeight[NMAXVTX];
+      int   bestMatchGenByTracks[NMAXVTX];
+      float contaminationFracByTracks[NMAXVTX];
       float trackWeightVtx[NMAXVTX][NMAXTRACKSVTX];
       float trackPtVtx[NMAXVTX][NMAXTRACKSVTX];
       float trackEtaVtx[NMAXVTX][NMAXTRACKSVTX];
@@ -117,9 +144,12 @@ class IsoVertexIDTreeAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedReso
       float trackXYErrVtx[NMAXVTX][NMAXTRACKSVTX];
       float trackZErrVtx[NMAXVTX][NMAXTRACKSVTX];
       bool  trackHPVtx[NMAXVTX][NMAXTRACKSVTX];
+      int  trackGenVertex[NMAXVTX][NMAXTRACKSVTX];
 
-      float meanPU_gen;
       int nVertices_gen;
+      std::vector< int > eventIDList;
+      std::vector< float > xVtx_gen;
+      std::vector< float > yVtx_gen;
       std::vector< float > zVtx_gen;
 
       void resetArrays();
@@ -151,8 +181,19 @@ isMC_(iConfig.getParameter<bool>("isMC"))
    token_vertices = consumes<std::vector<reco::Vertex>>(iConfig.getParameter<edm::InputTag>("VertexCollection"));
    token_tracks = consumes<std::vector<reco::Track>>(iConfig.getParameter<edm::InputTag>("TrackCollection"));
 
-   edm::InputTag PileupSrc_("addPileupInfo");
-   if(isMC_) token_genPU = consumes< std::vector< PileupSummaryInfo > >(PileupSrc_);
+   edm::InputTag TrackingVertexSrc_("mix","MergedTrackTruth");
+   edm::InputTag TrackingParticleSrc_("mix","MergedTrackTruth");
+   if(isMC_){
+     token_TrackingVtx = consumes< std::vector< TrackingVertex > >(TrackingVertexSrc_);
+     token_TrackingParticle = consumes< std::vector< TrackingParticle > >(TrackingParticleSrc_);
+     token_edmViewTracks = consumes<edm::View<reco::Track>>(iConfig.getParameter<edm::InputTag>("TrackCollection"));
+     associatorMapRTS_ = consumes< reco::RecoToSimCollection >(iConfig.getParameter<edm::InputTag>("associatorMap"));
+     associatorMapSTR_ = consumes< reco::SimToRecoCollection >(iConfig.getParameter<edm::InputTag>("associatorMap"));
+   }
+
+//   edm::InputTag PileupSrc_("addPileupInfo");
+//   if(isMC_) token_genPU = consumes< std::vector< PileupSummaryInfo > >(PileupSrc_);
+
 }
 
 
@@ -181,18 +222,82 @@ IsoVertexIDTreeAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup
    iEvent.getByToken(token_vertices, vertices);
    if(!vertices->size()) { std::cout<<"Invalid or empty vertex collection!"<<std::endl; vertexTree->Fill(); return; }
 
-/*
    edm::Handle< reco::TrackCollection > tracks;
    iEvent.getByToken(token_tracks, tracks);
    if(!tracks->size()) { std::cout<<"Invalid or empty track collection!"<<std::endl; return; }
-*/
 
    runNb = iEvent.id().run();
    eventNb = iEvent.id().event();
    lsNb = iEvent.luminosityBlock();
  
-   //get the GEN level Vertex info
+   //get the tracking Vertex info (truth for tracking purposes)
    if(isMC_){
+     edm::Handle< std::vector< TrackingVertex > > trkVtx;
+     iEvent.getByToken(token_TrackingVtx, trkVtx);
+
+     edm::Handle< std::vector< TrackingParticle > > trkPart;
+     iEvent.getByToken(token_TrackingParticle, trkPart);
+
+     std::vector< TrackingVertex >::const_iterator tv;
+     for( tv = trkVtx->begin(); tv != trkVtx->end(); ++tv){
+       if( tv->eventId().bunchCrossing()!=0) continue; //skip tracking vertices that are not in the main bunch crossing (reject Out-of-time PU)
+       if( tv->nSourceTracks() != 0 ) continue;//Skip vertices that decay from other stuff (not PVs)
+       if( fabs( tv->position().z() ) > 30) continue;// skip stuff >30cm in each direction
+
+       //make sure this is from a prompt process (not a g4 decay)
+       std::vector<SimVertex>::const_iterator g4Iter;
+       int processTotal = 0; 
+       for( g4Iter = tv->g4Vertices_begin(); g4Iter != tv->g4Vertices_end(); ++g4Iter){
+         processTotal += g4Iter->processType();//processes >0 are not prompt processes, so we don't want them
+       }
+       if(processTotal > 0) continue;
+       
+       //std::cout << tv->eventId().event() << " " <<  tv->daughterTracks().size() << std::endl;
+
+       //take the first tracking vertex from the event to be it's position
+       //Note: this may be slightly inaccurate if a PV splits into multiple tracking vertices, but it looks like the differences are <0.1cm in z on average
+       std::vector<int>::iterator it = std::find(eventIDList.begin(), eventIDList.end(), tv->eventId().event());     
+       if (it == eventIDList.end()){
+         eventIDList.push_back(tv->eventId().event());
+         xVtx_gen.push_back(tv->position().x());
+         yVtx_gen.push_back(tv->position().y());
+         zVtx_gen.push_back(tv->position().z());
+       }
+     }
+     nVertices_gen = eventIDList.size();    
+   }
+   
+   //track association maps (need to fix scoping here)
+   //This is ugly, but I cannot figure out how to use the RecoToSimCollections below in the vetex loop, so I build my own map...
+   std::map<long, int> trk2EvtMap; 
+   if(isMC_){ 
+     reco::RecoToSimCollection recSimColl;
+     reco::SimToRecoCollection simRecColl;
+     edm::Handle<reco::SimToRecoCollection > simtorecoCollectionH;
+     edm::Handle<reco::RecoToSimCollection > recotosimCollectionH;
+     iEvent.getByToken(associatorMapSTR_,simtorecoCollectionH);
+     simRecColl= *(simtorecoCollectionH.product());
+     iEvent.getByToken(associatorMapRTS_,recotosimCollectionH);
+     recSimColl= *(recotosimCollectionH.product()); 
+
+     Handle<edm::View<reco::Track> > tcol;
+     iEvent.getByToken(token_edmViewTracks, tcol);
+     for(edm::View<reco::Track>::size_type i=0; i<tcol->size(); ++i){
+       edm::RefToBase<reco::Track> trk(tcol, i);
+
+       std::vector<std::pair<TrackingParticleRef, double> > tp;
+       const TrackingParticle *mtp=0;
+       if(recSimColl.find(trk) != recSimColl.end())
+       {
+         tp = recSimColl[trk];
+         mtp = tp.begin()->first.get();
+         trk2EvtMap.insert(std::pair<long, int>( trkIdMaker(trk->pt(), trk->eta(), trk->phi(), trk->vz(), trk->chi2() ) , mtp->eventId().event())  );
+         //std::cout << mtp->eventId().event() << std::endl;
+       }
+     }
+   }
+
+   /*if(isMC_){
      edm::Handle<std::vector< PileupSummaryInfo > >  PupInfo;
      iEvent.getByToken(token_genPU, PupInfo);
 
@@ -205,7 +310,7 @@ IsoVertexIDTreeAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup
        }
        //std::cout << " Pileup Information: bunchXing, nvtx: " << PVI->getBunchCrossing() << " " << PVI->getPU_NumInteractions() << std::endl;
      }
-   }
+   }*/
 
    nVertices=0;
 
@@ -225,6 +330,26 @@ IsoVertexIDTreeAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup
        zVtxErr[nVertices] = vtx.zError();
        chi2Vtx[nVertices] = vtx.chi2();
        ndofVtx[nVertices] = vtx.ndof();
+      
+       for(unsigned int iv2=0; iv2<vertices->size(); iv2++)
+       {
+         if(iv==iv2) continue;
+         const reco::Vertex & vtx2 = (*vertices)[iv2];
+         if(!vtx2.isFake() && vtx2.tracksSize()>=2)
+         {
+           float zSep = fabs(vtx.z()-vtx2.z());
+           if(zSep < minZSepReco[nVertices]) minZSepReco[nVertices] = zSep;
+         }
+       }
+
+       float weightNet = 0;
+       float trackNet = 0;
+       std::vector< float > weightTotals;
+       std::vector< float > trackTotals;
+       if(isMC_){
+         for(size_t i = 0; i<eventIDList.size(); i++) weightTotals.push_back(0);
+         for(size_t i = 0; i<eventIDList.size(); i++) trackTotals.push_back(0);
+       }
 
        uint nTracksTmp=0;
        for (reco::Vertex::trackRef_iterator iTrack = vtx.tracks_begin(); iTrack != vtx.tracks_end(); iTrack++) {
@@ -244,7 +369,24 @@ IsoVertexIDTreeAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup
           trackHPVtx[nVertices][nTracksTmp] = track->quality(reco::TrackBase::highPurity);
 //std::cout<<nVertices<<" "<<zVtx[nVertices]<<" "<<nTracksTmp<<" "<<trackPtVtx[nVertices][nTracksTmp]<<" "<<trackZVtx[nVertices][nTracksTmp]<<std::endl;
 
-          if(isMC_) isMC_ = true;
+          if(isMC_){
+            int eventNumber = -1;
+            long ID = trkIdMaker(track->pt(), track->eta(), track->phi(), track->vz(), track->chi2() );
+            std::map<long,int>::iterator isFound = trk2EvtMap.find(ID);
+            if(isFound != trk2EvtMap.end() ){
+              eventNumber = (isFound)->second;
+              
+              //std::cout << "Found Number " << eventNumber << std::endl;
+              //std::cout << "Distance " << std::distance( eventIDList.begin(), std::find( eventIDList.begin(), eventIDList.end(), eventNumber) ) << std::endl;
+              int index = std::distance( eventIDList.begin(), std::find( eventIDList.begin(), eventIDList.end(), eventNumber) );
+              trackGenVertex[nVertices][nTracksTmp] = index;
+              
+              weightNet += vtx.trackWeight(*iTrack);
+              trackNet ++; 
+              weightTotals.at(index) += vtx.trackWeight(*iTrack);
+              trackTotals.at(index)++;
+            }
+          }
 
           if(isSlim_)
           {
@@ -255,6 +397,18 @@ IsoVertexIDTreeAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup
           }
           nTracksTmp++;
        }
+
+       if(isMC_){
+         for(size_t i = 0; i<eventIDList.size(); i++) weightTotals.at(i) = weightTotals.at(i)/weightNet;
+         for(size_t i = 0; i<eventIDList.size(); i++) trackTotals.at(i) = trackTotals.at(i)/trackNet;
+         std::vector< float >::iterator maxEle = std::max_element(weightTotals.begin(), weightTotals.end());
+         bestMatchGenByWeight[nVertices] = std::distance( weightTotals.begin(), maxEle);
+         contaminationFracByWeight[nVertices] = 1 - *maxEle;
+         std::vector< float >::iterator maxEleTrk = std::max_element(trackTotals.begin(), trackTotals.end());
+         bestMatchGenByTracks[nVertices] = std::distance( trackTotals.begin(), maxEleTrk);
+         contaminationFracByTracks[nVertices] = 1 - *maxEleTrk;
+       }
+
        nTracks[nVertices] = nTracksTmp;
 
        nVertices++;
@@ -287,7 +441,14 @@ IsoVertexIDTreeAnalyzer::resetArrays()
     chi2Vtx[i] = -999.0;
     ndofVtx[i] = -999.0;
     nTracks[i] = -999;
-    
+    if(isMC_){
+      contaminationFracByWeight[i] = -1;   
+      contaminationFracByTracks[i] = -1;   
+      bestMatchGenByWeight[i] = -1; 
+      bestMatchGenByTracks[i] = -1; 
+    }
+    minZSepReco[i] = 999.0;
+
     for(int j=0;j<NMAXTRACKSVTX;j++)
     {
       trackWeightVtx[i][j] = -999.0;
@@ -301,11 +462,14 @@ IsoVertexIDTreeAnalyzer::resetArrays()
       trackXYErrVtx[i][j] = -999.0;
       trackZErrVtx[i][j] = -999.0;
       trackHPVtx[i][j] = -999.0;
+      if(isMC_) trackGenVertex[i][j] = -1;
     }
   }
   if(isMC_){
-    meanPU_gen = -999;
     nVertices_gen = -999;
+    eventIDList.clear();
+    xVtx_gen.clear();
+    yVtx_gen.clear();
     zVtx_gen.clear();
   }
 }
@@ -326,6 +490,7 @@ IsoVertexIDTreeAnalyzer::beginJob()
   vertexTree->Branch("trackPtVtx",trackPtVtx,"trackPtVtx[nVertices][300]/F");
   vertexTree->Branch("trackEtaVtx",trackEtaVtx,"trackEtaVtx[nVertices][300]/F");
   vertexTree->Branch("trackPhiVtx",trackPhiVtx,"trackPhiVtx[nVertices][300]/F");
+  vertexTree->Branch("minZSepReco",minZSepReco,"minZSepReco[nVertices]/F");
 
   if(!isSlim_)
   {
@@ -344,10 +509,21 @@ IsoVertexIDTreeAnalyzer::beginJob()
     vertexTree->Branch("trackHPVtx",trackHPVtx,"trackHPVtx[nVertices][300]/O");
   }
   if(isMC_){
-    vertexTree->Branch("meanPU_gen",&meanPU_gen,"meanPU_gen/F");
+    vertexTree->Branch("bestMatchGenByWeight",bestMatchGenByWeight,"bestMatchGenByWeight[nVertices]/I");
+    vertexTree->Branch("contaminationFracByWeight",contaminationFracByWeight,"contaminationFracByWeight[nVertices]/F");
+    vertexTree->Branch("bestMatchGenByTracks",bestMatchGenByTracks,"bestMatchGenByTracks[nVertices]/I");
+    vertexTree->Branch("contaminationFracByTracks",contaminationFracByTracks,"contaminationFracByTracks[nVertices]/F");
+    vertexTree->Branch("trackGenVertex",trackGenVertex,"trackGenVertex[nVertices][300]/I");
     vertexTree->Branch("nVertices_gen",&nVertices_gen,"nVertices_gen/I");
+    vertexTree->Branch("xVtx_gen",&xVtx_gen);
+    vertexTree->Branch("yVtx_gen",&yVtx_gen);
     vertexTree->Branch("zVtx_gen",&zVtx_gen);
   }
+}
+
+long IsoVertexIDTreeAnalyzer::trkIdMaker( float pt, float eta, float phi, float vz, float chi2 ){
+  long ID = (int) (pt*100000) + (int)(fabs(eta)*10000) + (int)(fabs(phi)*1000) + (int)(fabs(vz)*100 + (int)chi2);
+  return ID;
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
--- IsoVertexIDAnalyzer/python/vertextree_cfi.py
+++ IsoVertexIDAnalyzer/python/vertextree_cfi.py
@@ -5,6 +5,8 @@ vertextree_ana = cms.EDAnalyzer('IsoVertexIDTreeAnalyzer',
   TrackCollection = cms.InputTag('generalTracks'),
   VertexCollection = cms.InputTag('offlinePrimaryVertices'),
 
+  associatorMap = cms.InputTag('tpRecoAssocGeneralTracks'),
+
   isSlim = cms.bool(False),
   isMC = cms.bool(False)
 )
