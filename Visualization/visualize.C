ee1ed772e5247f2f198e03010801ddadba895c8e
Austin <abaty@rice.edu>
2020-02-05 03:02:32 -0600

    changes to visualize

--- Visualization/visualize.C
+++ Visualization/visualize.C
@@ -10,7 +10,7 @@
 #include <vector>
 #include <algorithm>
  
-void visualization(int eventNumber, std::string fileName, std::string tag = "", float maxZ = 10){
+void visualization(int eventNumber, std::string fileName, std::string tag = "", float maxZ = 10, float maxY = 10){
 
   TFile * f = TFile::Open(fileName.c_str(),"read");
   TTree * t = (TTree*)f->Get("vertextree_ana/vertexTree");
@@ -24,6 +24,7 @@ void visualization(int eventNumber, std::string fileName, std::string tag = "", 
   int trackGenVertex[300][300] = {0};
   bool trackHPVtx[300][300] = {0};
   float contaminationFracByWeight[300] = {0};
+  int bestMatchGenByWeight[300] = {0};
 
   t->SetBranchAddress("zVtx_gen",&zVtx_gen); 
   t->SetBranchAddress("nVertices",&nVertices);
@@ -34,6 +35,7 @@ void visualization(int eventNumber, std::string fileName, std::string tag = "", 
   t->SetBranchAddress("trackGenVertex",trackGenVertex);
   t->SetBranchAddress("trackHPVtx",trackHPVtx);
   t->SetBranchAddress("contaminationFracByWeight",&contaminationFracByWeight);
+  t->SetBranchAddress("bestMatchGenByWeight",&bestMatchGenByWeight);
 
   t->GetEntry(eventNumber);
   
@@ -62,7 +64,7 @@ void visualization(int eventNumber, std::string fileName, std::string tag = "", 
 
   TH1D * topDummy = new TH1D("topDummy",";z (cm); GenTop",1,-maxZ,maxZ);
   topDummy->Fill(0.0010);
-  topDummy->GetYaxis()->SetRangeUser(-0.5,maxZ);
+  topDummy->GetYaxis()->SetRangeUser(-0.25,maxY);
   topDummy->GetYaxis()->SetTitle("Reco Radius (cm)");
   topDummy->GetYaxis()->CenterTitle();
   topDummy->Draw("p");
@@ -102,8 +104,16 @@ void visualization(int eventNumber, std::string fileName, std::string tag = "", 
   p1->cd();
   TLine * lR[300];
   std::vector< float > z;
+  std::vector< float > matchedIndexes;
   for(unsigned int i = 0; i<nVertices; i++){
     z.push_back(zVtx[i]);
+    matchedIndexes.push_back(bestMatchGenByWeight[i]);
+  }
+
+  int nSplit = 0;
+  for(unsigned int i = 0; i<zVtx_gen->size(); i++){
+    int n = std::count(matchedIndexes.begin(), matchedIndexes.end(),i);
+    if(n>1) nSplit++;
   }
   std::sort( z.begin(),z.end());
 
@@ -119,21 +129,21 @@ void visualization(int eventNumber, std::string fileName, std::string tag = "", 
       float theta = 2*TMath::ATan(TMath::Exp(-etaTrk[index][j]));
       float xStop = 0;
       float yStop = 3;
-      if( (theta<TMath::Pi()/2.0) && (TMath::Tan(theta) < maxZ/(maxZ-zTrk[index][j])) ){
+      if( (theta<TMath::Pi()/2.0) && (TMath::Tan(theta) < maxY/(maxZ-zTrk[index][j])) ){
         xStop = maxZ;
         yStop = TMath::Tan(theta) * (maxZ-zTrk[index][j]);
       }
       else if( theta<TMath::Pi()/2.0 ){
-        xStop = zTrk[index][j] + 1.0/TMath::Tan(theta)*(maxZ);
-        yStop = maxZ;
+        xStop = zTrk[index][j] + 1.0/TMath::Tan(theta)*(maxY);
+        yStop = maxY;
       } 
-      else if( (theta>TMath::Pi()/2.0) && (TMath::Tan(TMath::Pi()-theta) < maxZ/(TMath::Abs(zTrk[index][j] + maxZ))) ){
+      else if( (theta>TMath::Pi()/2.0) && (TMath::Tan(TMath::Pi()-theta) < maxY/(TMath::Abs(zTrk[index][j] + maxZ))) ){
         xStop = -maxZ;
         yStop = TMath::Tan(TMath::Pi()-theta) * (TMath::Abs(zTrk[index][j] + maxZ));
       }
       else if( theta>TMath::Pi()/2.0 ){
-        xStop = zTrk[index][j] - 1.0/TMath::Tan(TMath::Pi()-theta)*(maxZ) ;
-        yStop = maxZ;
+        xStop = zTrk[index][j] - 1.0/TMath::Tan(TMath::Pi()-theta)*(maxY) ;
+        yStop = maxY;
       }
       else{
         std::cout << "uh oh" << std::endl;
@@ -149,26 +159,59 @@ void visualization(int eventNumber, std::string fileName, std::string tag = "", 
   }
   TMarker * mR[300];
   TMarker * mR_noContam[300];
+  TMarker * mR_Split[300];
+  int uncontam = 0;
+  int nRecoSplit = 0;
+  int nUncontamNoSplit = 0;
   for(unsigned int i = 0; i<z.size(); i++){
+    bool isUncontam = false;
+    bool isSplit = false;
     int index = -1;
     for(unsigned int j = 0; j<z.size(); j++){
       if( TMath::Abs( zVtx[j] - z.at(i) ) < 0.001) index = j;
     }
     mR[i] = new TMarker(zVtx[index], 0, 8);
     mR[i]->SetMarkerColor(i%2+1);
-    mR[i]->SetMarkerSize(1.4);
+    mR[i]->SetMarkerSize(1.5);
     mR[i]->Draw("same");
     if(contaminationFracByWeight[index] < 0.0001){
-      std::cout << "hi" << std::endl;
-      mR_noContam[i] = new TMarker(zVtx[index], 0, 8);
+      mR_noContam[i] = new TMarker(zVtx[index], -0.1, 8);
       mR_noContam[i]->SetMarkerColor(kGreen+1);
-      mR_noContam[i]->SetMarkerSize(1.1);
-      mR_noContam[i]->Draw("same");    
+      mR_noContam[i]->SetMarkerSize(0.9);
+      mR_noContam[i]->Draw("same");   
+      uncontam++; 
+      isUncontam = true;
     }
+    
+    int splitNum = std::count(matchedIndexes.begin(), matchedIndexes.end(), bestMatchGenByWeight[index]);
+    if(splitNum>1){
+      mR_Split[i] = new TMarker(zVtx[index], -0.17, 8);
+      mR_Split[i]->SetMarkerColor(kMagenta);
+      mR_Split[i]->SetMarkerSize(0.9);
+      mR_Split[i]->Draw("same");   
+      nRecoSplit++;
+      isSplit = true;
+    }
+    if(!isSplit && isUncontam) nUncontamNoSplit++;
   } 
+
+  TLegend * leg = new TLegend(0.6,0.6,0.9,0.9);
+  leg->AddEntry((TObject*)0,Form("%d Gen Vtxs",(int)zVtx_gen->size()),"");
+  leg->AddEntry((TObject*)0,Form("%d Split Gen Vtxs",(int)nSplit),"");
+  leg->AddEntry((TObject*)0,Form("%d Reco Vtxs",(int)nVertices),"");
+  leg->AddEntry((TObject*)0,Form("%d Uncontaminated Vtxs",(int)uncontam),"");
+  leg->AddEntry((TObject*)0,Form("%d Split Reco Vtxs",(int)nRecoSplit),"");
+  leg->AddEntry((TObject*)0,Form("%d Uncontam+Unsplit Reco Vtxs",(int)nUncontamNoSplit),"");
+  leg->Draw("same");
 
   c1->SaveAs(Form("visualization_Evt%d_Tag%s.png",eventNumber, tag.c_str()));
   c1->SaveAs(Form("visualization_Evt%d_Tag%s.pdf",eventNumber, tag.c_str()));
   c1->SaveAs(Form("visualization_Evt%d_Tag%s.C",eventNumber, tag.c_str()));
 
+}
+
+void loopVisualization(int nEvents, std::string fileName, std::string tag = "", float maxZ = 10, float maxY = 10){
+ for(int i = 0; i<nEvents; i++){
+   visualization(i, fileName, tag+"_"+std::to_string(i), maxZ, maxY);
+ }
 }
